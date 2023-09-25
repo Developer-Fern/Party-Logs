d50f71cf50d0b68b16cf4d9f1b77253d296c3d6b
Fern <FernandoFlores.private@gmail.com>
2023-09-24 21:46:21 -0400

    added render party functionality

--- script.js
+++ script.js
@@ -129,7 +129,9 @@ const renderParties = async (parties) => {
 
 // init function
 const init = async () => {
-  // your code here
+
+const parties = await getAllParties()
+renderParties(parties)
 };
 
 init();
